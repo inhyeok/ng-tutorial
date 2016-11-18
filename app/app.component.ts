b52f0e90cc9a73750af1e57a363815b7c232019d
Jeong Inhyeok <inhyeok0327@gmail.com>
2016-11-18 21:55:59 +0900

    테스트 input two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello Angular</h1>`
+  template: `
+    <h1>Hello {{name}}</h1>
+    <input type="text" [(ngModel)]="name" />
+  `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  name: string;
+  name = "구스파치";
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { Nav } from './nav';
@@ -10,6 +11,7 @@ import { routes } from './app.routes';
 @NgModule({
   imports:      [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes, {
       useHash: true
     })
