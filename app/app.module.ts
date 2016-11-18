f79ba8d1c58f4b369fba7e1c3b715aa193f5106f
Jeong Inhyeok <inhyeok0327@gmail.com>
2016-11-18 17:58:37 +0900

    라우팅 기본 코드 추가

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 
+const appRoutes: Routes = [
+  // { path: "/", component: exComponent}
+]
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
