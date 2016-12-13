15a2deb8ad41c96e9b4763603b7040918682374a
Jeong Inhyeok <inhyeok0327@gmail.com>
2016-12-14 00:50:24 +0900

    test message 입력 값 넣기 추가

--- app/chat.ts
+++ app/chat.ts
@@ -3,8 +3,43 @@ import { ChatService } from './chat.service'
 
 @Component({
   selector: 'chat',
-  template: `<h2>chat</h2>`,
+  template: `
+    <h2>chat</h2>
+    <div class="container chat">
+      <div class="chat-item" *ngFor="let message of message_list">
+        <small>{{message.user_id}}</small>
+        <span>{{message.text}}</span>
+      </div>
+    </div>
+    <form (ngSubmit)="sendMessage()">
+      <textarea [(ngModel)]="message" name="message"></textarea>
+      <button type='submit' [disabled]="!message">보내기</button>
+      <button type='button' (click)="testAddMessage(message)">테스트 보내기</button>
+    </form>
+  `,
   providers: [ChatService]
 })
 
-export class Chat { }
+export class Chat {
+  message_list:any = []
+  message:string = ''
+
+  sendMessage(message) {
+    console.log('send Message submit!!', message)
+    // todo: socket send Message
+    return true;
+  }
+  addMessage(data) {
+    console.log('add Message', data)
+    this.message_list.push(data)
+    this.message = ''
+    return true;
+  }
+  testAddMessage(message) {
+    var data:any = {
+      'user_id': 1,
+      'text': message
+    }
+    this.addMessage(data)
+  }
+}
